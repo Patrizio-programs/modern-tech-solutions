57425a141d25aef7822697724460fd50ae874a79
Patrizio-programs <73086462+Patrizio-programs@users.noreply.github.com>
2022-05-15 18:36:43 -0400

    Upload

---
+++ src/footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+function Footer() {
+
+    return <div>
+
+        <footer>
+
+            <h3>Modern Tech Solutions</h3>
+
+            <div className="social">
+                <i class="fa-brands fa-facebook fa-flip"></i>
+                <i class="fa-brands fa-instagram fa-flip"></i>
+            </div>
+        </footer>
+    </div>
+}
+
+export default Footer;
---
+++ src/nav.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+    
+    
+
+
+    return <div>
+
+        <nav className="clearfix">
+
+            <ul className={"nav-ul Ham-Menu hidden"}  > 
+
+                <li><Link to="/">Home</Link></li>
+                
+                <li><Link to="/Contact">Contact</Link></li>
+
+                <li><Link to="/Services">Services</Link></li>
+
+                            
+            </ul>
+        </nav>
+    </div>
+}
+
+export default Nav;
