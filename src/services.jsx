614542eab3e77e7f83eb2c9387915ed00ce9c61f
PatrizioTheDev <patickmedley@gmail.com>
2023-07-21 13:02:42 -0400

    gg

--- src/services.jsx
+++ src/services.jsx
@@ -20,10 +20,8 @@ function Services() {
 <h3>Product information</h3>
 
 <div className="card" tabindex="0">
-  <span className="card__infoicon">
-    <i className="fa fa-info fa-beat-fade"></i>
-  </span>
-  <h1 className="card__title">Product 1</h1>
+<div className="card__img"></div>
+<hr/> 
   <p className="card__description">Lorem ipsum dolor sit amet and this is all the lorem ipsum</p>
 </div>
 </div>
@@ -32,35 +30,25 @@ function Services() {
 <h3>Product information</h3>
 
 <div className="card" tabindex="0">
-  <span className="card__infoicon">
-    <i className="fa fa-info fa-beat-fade"></i>
-  </span>
-  <h1 className="card__title">Product 2</h1>
+<div className="card__img"></div> 
+<hr/> 
   <p className="card__description">Lorem ipsum dolor sit amet and this is all the lorem ipsum</p>
 </div>
 </div>
 
 <div className="card-container">
 
-    <h3>Product information</h3>
+<h3>Product information</h3>
 <div className="card" tabindex="0">
-
-  <span className="card__infoicon">
-    <i className="fa fa-info fa-beat-fade"></i>
-  </span>
-  <h1 className="card__title">Product 3</h1>
-  <p className="card__description">Lorem ipsum dolor sit amet and this is all the lorem ipsum</p>
+<div className="card__img"></div>
+<hr/> 
+<p className="card__description">Lorem ipsum dolor sit amet and this is all the lorem ipsum</p>
 </div>
 </div>
 
-
+    </div>
     </div>
 
-
-
-
-    </div>
-    
 }
 
 export default Services;
